acee0fa770ec0f99edb6c8ec347e7124c62bd701
DEVLUE <devlue92@gmail.com>
2020-02-02 22:54:26 +0900

    Gatsby-Remark-Images Webp 비활성화

--- gatsby-config.js
+++ gatsby-config.js
@@ -113,7 +113,7 @@ module.exports = {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 960,
-              withWebp: true,
+              withWebp: false,
               ignoreFileExtensions: [],
             }
           },
